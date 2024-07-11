eb97379c273282b7eff950664c8fb96ee21afa80
abtarim <ab_tarim@hotmail.com>
2024-07-12 01:14:13 +0300

    "subtraction commit 004 - Deleted files was restored"

---
+++ src/main/java/com/softesteam/repodemo/calcoperations/Addition.java
@@ -0,0 +1,11 @@
+package com.softesteam.repodemo.calcoperations;
+
+public class Addition {
+
+    public Addition() {
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+}

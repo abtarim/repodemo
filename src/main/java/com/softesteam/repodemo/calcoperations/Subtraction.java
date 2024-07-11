a3ccdf889fd8f7a465f444437d9e6ecf39e4facc
abtarim <ab_tarim@hotmail.com>
2024-07-11 19:12:04 +0300

    "subtraction commit 001"

--- src/main/java/com/softesteam/repodemo/calcoperations/Subtraction.java
+++ src/main/java/com/softesteam/repodemo/calcoperations/Subtraction.java
@@ -1,4 +1,10 @@
 package com.softesteam.repodemo.calcoperations;
 
 public class Subtraction {
+    public Subtraction() {
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
 }

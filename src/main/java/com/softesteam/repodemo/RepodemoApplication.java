950b83b0c460b2c664fb911d36456d7e7478e24a
abtarim <ab_tarim@hotmail.com>
2024-07-11 17:32:32 +0300

    first commit

---
+++ src/main/java/com/softesteam/repodemo/RepodemoApplication.java
@@ -0,0 +1,13 @@
+package com.softesteam.repodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepodemoApplication.class, args);
+	}
+
+}
